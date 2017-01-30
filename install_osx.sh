3ea36288e1ee5bc68c0e56fcee144e98ec95f09e
Are <arenonani@arenonani.com>
2017-01-30 23:24:57 +0900

    インストールパスを Photoshop CC 2017 に変更

--- install_osx.sh
+++ install_osx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SCRIPT_DIR="/Applications/Adobe Photoshop CC 2015.5/Presets/Scripts/"
+SCRIPT_DIR="/Applications/Adobe Photoshop CC 2017/Presets/Scripts/"
 sudo cp lib/*.jsx "$SCRIPT_DIR"
